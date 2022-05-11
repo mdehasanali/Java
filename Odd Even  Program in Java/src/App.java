c32822e431af01dfffcc8f45432f495b62a654d5
Md. Ehasan Ali <mdehasanali100@gmail.com>
2022-05-11 14:38:44 +0600

    Even odd program update

--- Odd Even  Program in Java/src/App.java
+++ Odd Even  Program in Java/src/App.java
@@ -1,5 +1,18 @@
+import java.util.Scanner;
+
 public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) {
+        Scanner reader = new Scanner(System.in);
+        System.out.println("Enter the Number");
+        int num = reader.nextInt();
+
+        if (num == 0) {
+            System.out.println(num + " Zero Number");
+        } else if ((num % 2) == 0) {
+            System.out.println(num + " is Even Number");
+        } else {
+            System.out.println(num + " is Odd Number");
+        }
+
     }
 }
